5bbc138790fc551838fab31af7e6312214e4ab4d
AliMahmoudi <alimahmoudi21a@gmail.com>
2024-09-22 13:57:02 +0330

    Select Elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// ** Select Elements **
+const leftEye = document.querySelector("#lefteye");
+const rightEye = document.querySelector("#righteye");
+const userInput = document.querySelector("#userInput");
+const passInput = document.querySelector("#passInput");
